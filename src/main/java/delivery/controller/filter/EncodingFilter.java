30d2a37cf913954ead85aeb775b0187f0eaaec06
alexandr_omelchenko <ooomelchenko@gmail.com>
2019-01-14 01:17:25 +0200

    clean LocaleFilter(); add EncodingFilter;

---
+++ src/main/java/delivery/controller/filter/EncodingFilter.java
@@ -0,0 +1,24 @@
+package delivery.controller.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "encodingFilter", urlPatterns = {"/*"})
+public class EncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig){
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletResponse.setContentType("text/html");
+        servletResponse.setCharacterEncoding("UTF-8");
+        servletRequest.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- src/main/java/delivery/controller/filter/SessionLocaleFilter.java
+++ src/main/java/delivery/controller/filter/SessionLocaleFilter.java
@@ -7,34 +7,33 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Locale;
+import java.util.Optional;
 
 @WebFilter(filterName = "SessionLocaleFilter", urlPatterns = {"/*"})
 public class SessionLocaleFilter implements Filter {
-
+    @Override
     public void init(FilterConfig arg0) throws ServletException {
     }
-
+    @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
         HttpServletRequest req = (HttpServletRequest) request;
 
-        String locale= req.getParameter("sessionLocale");
+        String locale = req.getParameter("sessionLocale");
 
         if (locale != null) {
             req.getSession().setAttribute("lang", locale);
-            LocaleThreadLocal.setLocale(new Locale(locale));
+            LocaleThreadLocal.setLocale(Locale.forLanguageTag(locale));
         }
-        else {
-            try {
-                LocaleThreadLocal.setLocale(new Locale((String) req.getSession().getAttribute("lang")));
-            } catch (NullPointerException e) {
-                LocaleThreadLocal.setLocale(Locale.getDefault());
-            }
-        }
+        else
+            LocaleThreadLocal.setLocale(Locale.forLanguageTag(Optional
+                    .ofNullable((String) req.getSession().getAttribute("lang"))
+                    .orElse(Locale.getDefault().getLanguage())));
+
         chain.doFilter(request, response);
     }
-
+    @Override
     public void destroy() {}
 
 }
