9883740e0a06d3af972f21d719091f0f2c350874
alexandr_omelchenko <ooomelchenko@gmail.com>
2019-01-13 23:58:13 +0200

    fix LocalThread getLocale();

--- src/main/java/delivery/util/LocaleThreadLocal.java
+++ src/main/java/delivery/util/LocaleThreadLocal.java
@@ -1,12 +1,13 @@
 package delivery.util;
 
 import java.util.Locale;
+import java.util.Optional;
 
 public class LocaleThreadLocal {
     private static final ThreadLocal<Locale> threadLocalScope = new  ThreadLocal<>();
 
     public static Locale getLocale() {
-        return threadLocalScope.get();
+        return Optional.ofNullable(threadLocalScope.get()).orElse(Locale.getDefault());
     }
 
     public static void setLocale(Locale locale) {
